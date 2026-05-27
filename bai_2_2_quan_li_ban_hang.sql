9a792406bfd69feb14d4e3e5bacc0d7327eac510
Tominhtuan25 <rubynguyen.boyto@gmail.com>
2026-05-28 01:27:35 +0900

    bai 2_csdl_new

--- bai_2_2_quan_li_ban_hang.sql
+++ bai_2_2_quan_li_ban_hang.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE quan_li_ban_hang;
 USE quan_li_ban_hang;
-CREATE TABLE cutomers (
+CREATE TABLE customers (
 c_id INT AUTO_INCREMENT PRIMARY KEY,
 c_name NVARCHAR(50) NOT NULL,
 c_age INT
@@ -12,16 +12,16 @@ p_price DECIMAL(10,2)
 );
 CREATE TABLE orders (
 o_id INT AUTO_INCREMENT PRIMARY KEY,
-c_id INT AUTO_INCREMENT,
+c_id INT,
 o_date DATE,
 o_total_price DECIMAL(10,2),
 FOREIGN KEY (c_id) REFERENCES customers (c_id)
 );
 CREATE TABLE order_details (
 o_id INT,
-P_id INT,
+p_id INT,
 od_qty INT,
 PRIMARY KEY (o_id,P_id),
 FOREIGN KEY (o_id) REFERENCES orders (o_id),
-FOREIGN KEY (p_id) REFERENCES product (p_id)
+FOREIGN KEY (p_id) REFERENCES products (p_id)
 );
