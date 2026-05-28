310f39f4dba308a15e1624b6416d0962424633fe
Tominhtuan25 <rubynguyen.boyto@gmail.com>
2026-05-28 20:50:37 +0900

    bai 2 csdl_update_loi

--- bai_2_1_chuyen_erd_sang_mo_hinh_quan_he.sql
+++ bai_2_1_chuyen_erd_sang_mo_hinh_quan_he.sql
@@ -1,20 +1,25 @@
 CREATE DATABASE chuyen_doi_erd_sang_mo_hinh_quan_he;
-USE chuyen_doi_ERD_sang_mo_hinh_quan_he;
+USE chuyen_doi_erd_sang_mo_hinh_quan_he;
 CREATE TABLE vattu (
 ma_vtu INT PRIMARY KEY,
 ten_vtu VARCHAR(100) NOT NULL
+);
+CREATE TABLE nhacc (
+ma_ncc INT PRIMARY KEY,
+ten_ncc NVARCHAR(50) NOT NULL,
+dia_chi NVARCHAR(150)
+);
+CREATE TABLE so_dien_thoai_nhacc (
+ma_sdt INT AUTO_INCREMENT PRIMARY KEY,
+ma_ncc INT,
+sdt VARCHAR(20),
+FOREIGN KEY (ma_ncc) REFERENCES nhacc (ma_ncc)
 );
 CREATE TABLE dondh (
 so_dh INT PRIMARY KEY,
 ngay_dh DATE,
 ma_ncc INT,
 FOREIGN KEY (ma_ncc) REFERENCES nhacc(ma_ncc)
-);
-CREATE TABLE nhacc (
-ma_ncc INT PRIMARY KEY,
-ten_ncc NVARCHAR(50) NOT NULL,
-dia_chi NVARCHAR(150),
-sdt VARCHAR(20)
 );
 CREATE TABLE phieu_xuat (
 so_px INT PRIMARY KEY,
@@ -31,7 +36,7 @@ dg_xuat DECIMAL(10,2),
 sl_xuat INT,
 PRIMARY KEY (so_px, ma_vtu),
 FOREIGN KEY (so_px) REFERENCES phieu_xuat (so_px),
-FOREIGN KEY (ma_vt) REFERENCES vattu (ma_vt)
+FOREIGN KEY (ma_vtu) REFERENCES vattu (ma_vtu)
 );
 CREATE TABLE chi_tiet_phieu_nhap (
 so_pn INT,
@@ -40,12 +45,12 @@ dg_nhap DECIMAL(10,2),
 sl_nhap INT,
 PRIMARY KEY (so_pn, ma_vtu),
 FOREIGN KEY (so_pn) REFERENCES phieu_nhap (so_pn),
-FOREIGN KEY (ma_vt) REFERENCES vattu (ma_vt)
+FOREIGN KEY (ma_vtu) REFERENCES vattu (ma_vtu)
 );
 CREATE TABLE chi_tiet_don_dh (
 so_dh INT,
 ma_vtu int,
 PRIMARY KEY (so_dh, ma_vtu),
 FOREIGN KEY (so_dh) REFERENCES dondh (so_dh),
-FOREIGN KEY (ma_vt) REFERENCES vattu (ma_vt)
+FOREIGN KEY (ma_vtu) REFERENCES vattu (ma_vtu)
 );
